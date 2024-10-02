214416d5a7ac905ad459698c1c7eeab5c89aa5a2
Kamaleshmlp <126389754+Kamaleshmlp@users.noreply.github.com>
2024-10-02 09:35:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = environ.get('START_IMG', 'https://i.ibb.co/qpxpGmC/image.jpg')
+START_IMG = environ.get('START_IMG', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq8pvhe3kiB5-jx_ss40anyqwFywyoUypIlw&usqp=CAU')
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
@@ -105,7 +105,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://jisshu-filter-bot-1vfp.onrender.com/")
+URL = environ.get("FQDN", "https://interested-gerry-autourl-af094cfa.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
